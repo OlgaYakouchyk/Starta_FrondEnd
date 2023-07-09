c74f22c81d7839feaba1f2015f0236953c3ce6b7
Olga <Kraisler60@ya.ru>
2023-07-09 12:39:59 -0400

    Lesson7

---
+++ Lesson7/script.js
@@ -0,0 +1,56 @@
+// console.log("Hello world");
+
+// const myName = "Olga";
+// const myAge = 38;
+// const IsProgrammer = true;
+// const myFamily = {
+//   mom: "Natallia",
+//   dad: "Vladimir",
+//   bro: "Yra",
+//   sister: "Anna",
+//   cat: "lily",
+// };
+// const myInfo = "my name is/..............";
+// console.log(myFamily);
+// console.log("Hello my name is " + myName + " I am " + myAge + " years old");
+
+// const myCity = "Philadelphia";
+// const population = 1500000;
+
+// console.log(
+//   "hi, my name is " +
+//     myName +
+//     ", I living in " +
+//     myCity +
+//     ", the population is " +
+//     population
+// );
+// console.log(`hi, my nami is ${myName}, my citi is ${myCity}`);
+
+// const r =12;
+// const g =34;
+// const b =14;
+// const result = `rgb(${r}, ${g}, ${b})`;
+// console.log(result);
+
+// console.log('b'+'a'+ +'c');
+
+// const firstRequest= prompt('Enter your name')
+// console.log(firstRequest);
+// const secondRequest= prompt('Enter your age')
+// console.log(Math.pow(secondRequest));
+
+const a = prompt('Enter your  first number')
+const b = prompt('Enter your  second number')
+
+console.log(parseFloat(a)+ parseFloat(b));
+
+
+
+
+
+
+
+
+
+
