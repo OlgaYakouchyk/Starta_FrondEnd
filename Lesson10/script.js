09eab737ab0382da884e40609837c18fa82d9e8b
Olga <Kraisler60@ya.ru>
2023-08-03 18:58:03 -0400

    Lesson10

---
+++ Lesson10/script.js
@@ -0,0 +1,64 @@
+//const arrA = [1, 2, 3, 4, "hello", "my", "friends"];
+// console.log(arr);
+// for (i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// function qwerty (arr) {
+//     for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+
+// }
+// }
+//   qwerty(arrA);
+
+// function fn(myName, lastName, x, y) {
+//     if(myName == 'Olga'){
+//         console.log('Hello Olga!');
+
+//     }
+//   return x + y;
+// }
+
+// console.log(fn('Olga', "Yakouchyk", 5, 4));
+//_________________________________________________
+// const box = document.querySelector('.box')
+// console.log(box);
+// const item = document.querySelectorAll('.menu_item')
+// console.log(item);
+// const boxWhith = document.querySelector('#box')
+//___________________________________________________
+
+// const main = document.querySelector(".main");
+// main.innerHTML = 'Hello my friends!';
+// console.log(main);
+//  const newElem = document.createElement('div');
+//  newElem.innerText = 'This is just created DIV';
+//  document.body.append(newElem);
+
+// const menu = document.querySelector('.menu')
+// console.log(menu);
+// const newMenuItem = document.createElement('li');
+// menu.append(newMenuItem);
+// newMenuItem.innerText = 'This is last element'
+
+//___________________________________________
+
+// const paragraph = document.querySelectorAll('.paragrhp');
+// console.log(paragraph);
+
+// for (i = 0; i < paragraph.length; i++) {
+//   paragraph[i].innerText = 'any';
+//   console.log(paragraph[i]);
+// }
+//___________________________________________________________
+
+// const conteiner = document.querySelector('#conteiner');
+// const newSection = document.createElement('section');
+// const newDiv = document.createElement('div');
+// newDiv.innerText = 'Hello world';
+// conteiner.append(newSection);
+// newSection.append(newDiv);
+
+const img = document.querySelector('.img')
+img.setAttribute('src', './robots1.jpg')
